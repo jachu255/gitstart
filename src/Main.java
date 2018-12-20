8ab681670a3c899e12fa196bf81994897f9a33fc
Jan255 <jachu255@gmail.com>
2018-12-20 22:09:58 +0100

    Usunięcie kolekcji

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 import com.jan255.*;
 
-import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        List<String> listWithNames = new LinkedList<>();
-        listWithNames.add("Piotrek");
-        listWithNames.add("Krzysiek");
-
-        System.out.println("Hello World! "+listWithNames.size());
-
-        Set<String> setWithNames = new HashSet<>();
-        setWithNames.add("Marcin");
-        setWithNames.add("Marek");
 
         Numer1 x = new Numer1();
         Numer2 y = new Numer2();
