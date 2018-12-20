d5912010e8b5cd868e135c4f5149d93827372251
Jan255 <jachu255@gmail.com>
2018-12-20 21:52:41 +0100

    Kolekcje w java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! 123456");
+
+        List<String> listWithNames = new LinkedList<>();
+        listWithNames.add("Piotrek");
+        listWithNames.add("Krzysiek");
+
+        System.out.println("Hello World! "+listWithNames.size());
+
+        Set<String> setWithNames = new HashSet<>();
+        setWithNames.add("Marcin");
+        setWithNames.add("Marek");
+
     }
 }
